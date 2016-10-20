e6ff12ed666a25afad3f5ae58edcf086f677e19a
Xingye(Ryan) Zhang <fly.ryanzh@gmail.com>
2016-10-20 18:10:03 -0400

    Delete instagram.py

--- code/instagram.py
+++
@@ -1,7 +0,0 @@
-import urllib2
-import re
-
-
-def get_ins_username(pic_link):
-	web_page = urllib2.urlopen(pic_link).read()
-	return re.search('"owner": {"username": "(.*)", "is_unpublished"', web_page).groups()[0]
